a6f044bf656158572c4b65d8b5544600bf62f79c
Martin Kozlovsky <kozlovsky.m7@gmail.com>
2022-01-20 20:48:21 +0100

    workign clicks, faster

--- project.py
+++ project.py
@@ -71,7 +71,7 @@ STATES = {
 
 
 years = list(range(1992, 2016))
-states = list(set(frame['STATE']))
+states = list(STATES.keys())
 sts = []
 for state in states:
     sts.extend([state] * len(years))
@@ -81,70 +81,16 @@ geoframe = pd.DataFrame({
 
 })
 
-def get_center(df):
-    lon_min = df['LONGITUDE'].min()
-    lon_max = df['LONGITUDE'].max()
-    lat_min = df['LATITUDE'].min()
-    lat_max = df['LATITUDE'].max()
-    return {'lat': (lat_min + lat_max) / 2,
-            'lon': (lon_min + lon_max) / 2}
 
-
-# with open('./geo.json', 'r', encoding='utf-8') as file:
-    # geojson = json.load(file)
-
-
-app.layout = html.Div(children=[
-    html.H1(children='Wildfires in the United States from 1992 to 2015.'),
-    dcc.Checklist(
-        id='is_year_range',
-        options=[
-            {'label': 'Custom year range', 'value': 'True'},
-        ],
-        value=['True']
-    ),
-    dcc.RangeSlider(
-        id='year_range',
-        marks={i: str(i) for i in range(1992, 2016)},
-        min=1992,
-        max=2015,
-        value=[1992, 2015]
-    ),
-    html.Div(id='container', children=[]),
-
-    dcc.Graph(id='graph')
-])
-
-last_click = None
-
-@app.callback(
-    [Output(component_id='graph', component_property='figure'),
-     Output(component_id='container', component_property='children')],
-    [Input(component_id='year_range', component_property='value'),
-     Input(component_id='graph', component_property='clickData')]
-)
-def update_graph(years, click):
-    global last_click
-    pprint(click)
-    state = None
-    if (click is not None
-            and ((loc:=click['points'][0]['location']) is not None
-                 or (custom:=click['points'][0].get('customdata')) is not None)):
-        st = loc or custom[0]
-        if st != last_click:
-            state = last_click = st
-    df = frame if state is None else frame[frame['STATE'] == state]
-    gf = geoframe if state is None else geoframe[geoframe['STATE'] == state]
-    container = f'Chosen state: {state}'
-    min_year, max_year = years
-    fig = px.choropleth(gf,
-                        animation_frame='YEAR',
-                        color='STATE',
-                        locations='STATE',
-                        color_discrete_map={state: '#EEEEFF' for state in STATES},
-                        locationmode="USA-states", scope="usa",
-                        width=1000,
-                        height=700)
+def make_graph(df, gf, focus):
+    fig = px.choropleth(
+        gf,
+        color='STATE',
+        locations='STATE',
+        color_discrete_map={state: '#EEEEFF' for state in STATES},
+        locationmode="USA-states", scope="usa",
+        width=1000,
+        height=700)
 
     fig.update_layout(showlegend=False)
     fig.update_traces(
@@ -162,8 +108,12 @@ def update_graph(years, click):
                                  opacity=0.6,
                                  width=1000,
                                  height=700,
-                                 animation_frame='YEAR',
                                  range_color=[0, 606945])
+
+    if focus:
+        fig.update_geos(fitbounds='locations')
+    else:
+        fig.update_geos(visible=False)
 
     fig_scatter.update_traces(marker=dict(line=dict(width=0)),
                               selector=dict(mode='markers'))
@@ -173,15 +123,86 @@ def update_graph(years, click):
             fig_scatter.data[0]
         )
 
-    for i in range(len(fig_scatter.frames)):
-        fig.frames[i].data += (fig_scatter.frames[i].data[0],)
-
-    if state is not None:
-        fig.update_geos(fitbounds='locations')
-
     fig.layout['coloraxis'] = fig_scatter.layout['coloraxis']
 
-    return fig, container
+    return fig
+
+
+def filter_year(df, min_year, max_year):
+    return df[(df['YEAR'] >= min_year) & (df['YEAR'] <= max_year)]
+
+
+DEFAULT_GRAPH = make_graph(
+    filter_year(frame, 2015, 2015),
+    filter_year(geoframe, 2015, 2015),
+    False)
+
+app.layout = html.Div(children=[
+    html.H1(children='Wildfires in the United States from 1992 to 2015.'),
+    dcc.RangeSlider(
+        id='year_range',
+        marks={i: str(i) for i in range(1992, 2016)},
+        min=1992,
+        max=2015,
+        value=[2015, 2015],
+        allowCross=False
+    ),
+    html.Button('Zoom back', id='button'),
+    html.Div(id='container', children=[]),
+
+    dcc.Graph(id='graph', figure=DEFAULT_GRAPH)
+])
+
+
+last_click = None
+prev_years = (2015, 2015)
+
+
+@app.callback(
+    [Output('graph', 'figure'),
+     Output('container', 'children')],
+    [Input('year_range', 'value'),
+     Input('graph', 'clickData'),
+     Input('button', 'n_clicks')]
+)
+def update_graph(years, click, button):
+    global last_click
+    global prev_years
+    global DEFAULT_GRAPH
+    if years != prev_years:
+        DEFAULT_GRAPH = make_graph(
+            filter_year(frame, *years),
+            filter_year(geoframe, *years),
+            False)
+    pprint(click)
+    ctx = dash.callback_context
+    # print(ctx.triggered)
+    # print(ctx.states)
+    # print(ctx.inputs)
+    if click is None or ctx.triggered[0]['prop_id'] == 'button.n_clicks':
+        if ctx.triggered[0]['prop_id'] != 'year_range.value':
+            return DEFAULT_GRAPH, 'Whole country'
+
+    if ((location := click['points'][0]['location']) is not None
+            or (location := click['points'][0]
+                .get('customdata', [None])[0]) is not None):
+
+        state = location
+    else:
+        raise dash.exceptions.PreventUpdate
+
+    if state == last_click:
+        last_click = None
+        return DEFAULT_GRAPH, 'Whole country'
+    else:
+        last_click = state
+
+    df = filter_year(frame[frame['STATE'] == state], *years)
+    gf = filter_year(geoframe[geoframe['STATE'] == state], *years)
+    container = f'Chosen state: {state}'
+
+    return make_graph(df, gf, True), container
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
